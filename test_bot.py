440c337322b8e2af1466aa57a09f674979397231
Jhonata Alves <jhonatab4@gmail.com>
2024-02-22 20:18:17 -0300

    feat: test commands

--- test_bot.py
+++ test_bot.py
@@ -4,28 +4,64 @@ import telebot
 
 load_dotenv()
 
+"""
+    /destino informe o aeroporto de embarque
+    /data_de_partida informe a data do voo de partida
+    /data_de_volta informe a data do voo de volta
+"""
+
 token = os.environ["token_telegram"]
 bot = telebot.TeleBot(token)
+url_base = f'api.telegram.org/bot{token}'
 
-@bot.message_handler(commands=["start"])
+# def msg_obtain(update_id):    
+#     msg_capture = f'{url_base}/getUpdates?timeout=100'
+#     if update_id:
+#         msg_capture = f'{msg_capture}/&offset={update_id + 1}'
+#     result = resquests.get(msg_capture)
+#     print(result)
+
+@bot.message_handler(commands=["iniciar"])
 def start(msg):
-    bot.reply_to(msg, "Vou lhe perguntar alguns dados do voo, por favor responda de acordo com os exemplos")
+    text = """
+        Vou lhe perguntar alguns dados do voo, por favor responda de acordo com os exemplos.
+        Clique no embarque para começarmos:
+        /embarque"""
+    
+    bot.send_message(msg.chat.id, text)
 
-@bot.message_handler(commands=["options"])
+@bot.message_handler(commands=["embarque"])
 def start(msg):
-    bot.reply_to(msg, "Vou lhe listar as opções")
+    text_info = """Digite as siglas do aeroporto de Embarque. Ex: Digite BEL para Belém ou GRU para Guarulhos"""
 
-def vefiry(msg):    
+    bot.reply_to(msg, text_info)
+
+@bot.message_handler(commands=["destino"])
+def start(msg):
+    departures = msg.text
+    bot.reply_to(msg, "Digite as siglas do aeroporto de Destino. Ex: Digite BEL para Belém ou GRU para Guarulhos")
+    
+    msg_obtain()
+
+# @bot.message_handler(commands=["data_de_partida"])
+# def start(msg):
+#     bot.reply_to(msg, "Digite a data de partida. Ex: 01/01/2024")
+
+# @bot.message_handler(commands=["data_de_volta"])
+# def start(msg):
+#     bot.reply_to(msg, "Digite a data de partida. Ex: 31/12/2024")
+
+def verify(msg):    
     return True
 
-@bot.message_handler(func=vefiry)
-def response(msg):
-    texto = """
-    Olá, sou o Flights Search, seu bot para encontrar passagens a custo benefício ^^:
-        /start inicia a inserção de dados
-        /options opões de voo
-    Responder qualquer outra coisa não vai funcionar. Clique na opção de inicio."""
+@bot.message_handler(func=verify)
+def response(msg):    
+    user = msg.chat.first_name
+    text_info = f"""
+    Olá {user}, sou o Flights Search, seu bot para encontrar passagens a custo benefício 😉:        
+    Responder qualquer outra coisa estará sujeito a falhas. Clique na opção de iniciar.
+    /iniciar"""
 
-    bot.reply_to(msg, texto)
+    bot.reply_to(msg, text_info)
 
 bot.polling()
