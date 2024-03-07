5726dc4e5a95b761cc52dba2e9c1a856da05a98d
Jhonata Alves <jhonatab4@gmail.com>
2024-03-07 20:16:40 -0300

    att: testing api body

--- api/main.py
+++ api/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 
-
 class Travel(BaseModel):
     departures: str | None = None
     date_departures: str | None = None
@@ -9,11 +8,30 @@ class Travel(BaseModel):
     date_arrivals: str | None = None    
     price_travel: float
 
-
 app = FastAPI()
 
+travels = {
+    1: {
+        "input_departures": "BEL",
+        "date_departure": "14/03/2024",
+        "input_arrivals": "GRU",
+        "date_arrivals": "20/03/2024",
+        "value_travels": 1000,
+    },
+    2: {
+        "input_departures": "CGH",
+        "date_departure": "23/03/2024",
+        "input_arrivals": "GIG",
+        "date_arrivals": "29/03/2024",
+        "value_travels": 2000,
+    }
+}
 
-@app.post("/travel/")
-async def create_item(travel: Travel):
-    travel_dict = travel.dict()    
-    return travel_dict
+@app.get("/")
+async def home():
+    return {"Travel": len(travels)}
+
+@app.get("/travels/{id_travel}")
+async def search_for_trip(id_travel: int):
+    return travels[id_travel]
+
